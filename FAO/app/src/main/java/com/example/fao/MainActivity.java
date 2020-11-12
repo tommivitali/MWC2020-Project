935e262403cfed497642a326eb3890378997688a
NanolathingStuff <silva.edoardo97@gmail.com>
2020-11-12 16:47:58 +0100

    added working stepcounter

--- FAO/app/src/main/java/com/example/fao/MainActivity.java
+++ FAO/app/src/main/java/com/example/fao/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.fao;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -8,12 +10,15 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.textfield.TextInputLayout;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -30,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView results;
     public int userH = 0, userW = 0; //user's weight and height
     private String[] languages = new String[] {"Italian", "English", "German"};
+
+    //for permissions
+    private static final int REQUEST_ACTIVITY_RECOGNITION_PERMISSION = 45;
+    private boolean runningQOrLater =
+            android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,9 +91,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 userH = Integer.parseInt(height.getText().toString());
                 userW = Integer.parseInt(weight.getText().toString());
-                results.setText(userH + " " + userW + " " + language.getText().toString(););
+                results.setText(userH + " " + userW + " " + language.getText().toString());
             }
         });*/
+        // Ask for activity recognition permission
+        if (runningQOrLater) {
+            getActivity();
+        }
     }
 
     @Override
@@ -98,5 +112,31 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    // Ask for permission
+    private void getActivity() {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACTIVITY_RECOGNITION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACTIVITY_RECOGNITION},
+                    REQUEST_ACTIVITY_RECOGNITION_PERMISSION);
+        } else {
+            return;        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_ACTIVITY_RECOGNITION_PERMISSION) {
+            if(grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getActivity();
+            } else {
+                Toast.makeText(this,
+                        R.string.step_permission_denied,
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- FAO/app/src/main/java/com/example/fao/ui/home/HomeFragment.java
+++ FAO/app/src/main/java/com/example/fao/ui/home/HomeFragment.java
@@ -1,10 +1,21 @@
 package com.example.fao.ui.home;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +24,285 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.fao.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    public Context context;
+    MaterialButton StartStop;
+
+    // Text view variables
+    public TextView stepsCountTextView;
+
+    // ACC sensors.
+    private Sensor mSensorACC;
+    private SensorManager mSensorManager;
+    private SensorEventListener listener;
+
+    // Step Detector sensor
+    private Sensor mSensorStepDetector;
+
+    //Completed steps
+    static int stepsCompleted = 0;
 
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // TODO: Get the number of steps stored in the current date
+        Date cDate = new Date();
+        String fDate = new SimpleDateFormat("yyyy-MM-dd").format(cDate);
+
+        stepsCountTextView = (TextView) root.findViewById(R.id.stepsCount);
+
+        // Set the Views with the number of stored steps
+        stepsCountTextView.setText(String.valueOf(stepsCompleted));
+/*        stepsCompleted = StepAppOpenHelper.loadSingleRecord(getContext(), fDate);
+
+        //Get the writable database
+        StepAppOpenHelper databaseOpenHelper =   new StepAppOpenHelper(this.getContext());;
+        SQLiteDatabase database = databaseOpenHelper.getWritableDatabase();*/
+
+        //  Get an instance of the sensor manager.
+        mSensorManager = (SensorManager) this.getActivity().getSystemService(Context.SENSOR_SERVICE);
+        mSensorACC = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        // Step detector instance
+        mSensorStepDetector = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+        // Instantiate the StepCounterListener
+        /*StepAppOpenHelper databaseOpenHelper =   new StepAppOpenHelper(this.getContext());;
+        SQLiteDatabase database = databaseOpenHelper.getWritableDatabase();*/
+        listener = new StepCounterListener(stepsCountTextView);
+
+        // Toggle button
+        StartStop = (MaterialButton) root.findViewById(R.id.buttonStartStopStepcounter);
+        StartStop.setChecked(true); //default stop counter
+        StartStop.addOnCheckedChangeListener(new MaterialButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(MaterialButton button, boolean isChecked) {
+
+                if (button.getId() == R.id.buttonStartStopStepcounter && !isChecked) {
+
+                    //Place code related to Start button
+                    Toast.makeText(getContext(), "START", Toast.LENGTH_SHORT).show();
+
+                    // Check if the Accelerometer sensor exists
+                    if (mSensorACC != null) {
+
+                        // Register the ACC listener
+                        mSensorManager.registerListener(listener, mSensorACC, SensorManager.SENSOR_DELAY_NORMAL);
+
+                    } else {
+                        Toast.makeText(getContext(), R.string.acc_not_available, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    // Check if the Step detector sensor exists
+                    if (mSensorStepDetector != null) {
+                        // Register the ACC listener
+                        mSensorManager.registerListener(listener, mSensorStepDetector, SensorManager.SENSOR_DELAY_NORMAL);
+
+                    } else {
+                        Toast.makeText(getContext(), R.string.step_not_available, Toast.LENGTH_SHORT).show();
+
+                    }
+                    //change text button
+                    StartStop.setText(R.string.home_stop_stepcounter);
+
+                } else if (button.getId() == R.id.buttonStartStopStepcounter && isChecked) {
+                    //Place code related to Stop button
+                    Toast.makeText(getContext(), "STOP", Toast.LENGTH_SHORT).show();
+
+                    // Unregister the listener
+                    mSensorManager.unregisterListener(listener);
+
+                    //change text button
+                    StartStop.setText(R.string.home_start_stop_stepcounter);
+                }
+            }
+        });
+
+
+
         return root;
+    }
+
+    @Override
+    public void onDestroyView (){
+        super.onDestroyView();
+        mSensorManager.unregisterListener(listener);
+    }
+
+    class StepCounterListener implements SensorEventListener {
+
+        private long lastUpdate = 0;
+
+        public int mACCStepCounter = HomeFragment.stepsCompleted;
+
+        ArrayList<Integer> mACCSeries = new ArrayList<Integer>();
+        ArrayList<String> mTimeSeries = new ArrayList<String>();
+
+        private double accMag = 0d;
+        private int lastXPoint = 1;
+        int stepThreshold = 10;
+
+        // Android step detector
+        public int mAndroidStepCounter = 0;
+
+        TextView stepsCountTextView;
+
+        //variables to graphs
+        SQLiteDatabase database;
+        public String timestamp;
+        public String day;
+        public String hour;
+
+        // Constructor, get the database, TextView and ProgressBar as args
+        public StepCounterListener(TextView tv){
+            stepsCountTextView = tv;
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+
+                // Case of the ACC
+                case Sensor.TYPE_LINEAR_ACCELERATION:
+
+                    // Get x,y,z
+                    float x = event.values[0];
+                    float y = event.values[1];
+                    float z = event.values[2];
+
+                    //////////////////////////// -- PRINT ACC VALUES -- ////////////////////////////////////
+                    // Timestamp
+                    long timeInMillis = System.currentTimeMillis() + (event.timestamp - SystemClock.elapsedRealtimeNanos()) / 1000000;
+
+                    // Convert the timestamp to date
+                    SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+                    //jdf.setTimeZone(TimeZone.getTimeZone("GMT+2"));
+                    String date = jdf.format(timeInMillis);
+
+
+                // print a value every 1000 ms
+                long curTime = System.currentTimeMillis();
+                if ((curTime - lastUpdate) > 1000) {
+                    lastUpdate = curTime;
+
+                    Log.d("ACC", "X: " + String.valueOf(x) + " Y: " + String.valueOf(y) + " Z: "
+                            + String.valueOf(z) + " t: " + String.valueOf(date));
+
+                }
+
+
+                    // Get the date, the day and the hour
+                    timestamp = date;
+                    day = date.substring(0,10);
+                    hour = date.substring(11,13);
+
+                    ////////////////////////////////////////////////////////////////////////////////////////
+
+                    /// STEP COUNTER ACC ////
+                    accMag = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+
+                    //Update the Magnitude series
+                    mACCSeries.add((int) accMag);
+                    //Update the time series
+                    mTimeSeries.add(timestamp);
+
+
+                    // Calculate ACC peaks and steps
+                    peakDetection();
+
+                    break;
+
+                // case Step detector
+                case Sensor.TYPE_STEP_DETECTOR:
+
+                    // Calculate the number of steps
+                    countSteps(event.values[0]);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            //
+        }
+
+
+        public void peakDetection() {
+            int windowSize = 20;
+
+            /* Peak detection algorithm derived from: A Step Counter Service for Java-Enabled Devices Using a Built-In Accelerometer, Mladenov et al.
+             */
+            int highestValX = mACCSeries.size(); // get the length of the series
+            if (highestValX - lastXPoint < windowSize) { // if the segment is smaller than the processing window skip it
+                return;
+            }
+
+            List<Integer> valuesInWindow = mACCSeries.subList(lastXPoint,highestValX);
+            List<String> timesInWindow = mTimeSeries.subList(lastXPoint,highestValX);
+
+            lastXPoint = highestValX;
+
+            int forwardSlope = 0;
+            int downwardSlope = 0;
+
+            List<Integer> dataPointList = new ArrayList<Integer>();
+            List<String> timePointList = new ArrayList<String>();
+
+
+            for (int p =0; p < valuesInWindow.size(); p++){
+                dataPointList.add(valuesInWindow.get(p)); // ACC Magnitude data points
+                timePointList.add(timesInWindow.get(p)); // Timestamps
+            }
+
+            for (int i = 0; i < dataPointList.size(); i++) {
+                if (i == 0) {
+                }
+                else if (i < dataPointList.size() - 1) {
+                    forwardSlope = dataPointList.get(i + 1) - dataPointList.get(i);
+                    downwardSlope = dataPointList.get(i)- dataPointList.get(i - 1);
+
+                    if (forwardSlope < 0 && downwardSlope > 0 && dataPointList.get(i) > stepThreshold ) {
+
+                        // Update the number of steps
+                        mACCStepCounter += 1;
+                        Log.d("ACC STEPS: ", String.valueOf(mACCStepCounter));
+
+                        // Update the TextView and the ProgressBar
+                        stepsCountTextView.setText(String.valueOf(mACCStepCounter));
+
+                        // Insert the data in the database
+                        //DONE 4: insert the day and the hour in the database
+                        ContentValues values = new ContentValues();
+                        /*values.put(StepAppOpenHelper.KEY_TIMESTAMP, timePointList.get(i));
+                        values.put(StepAppOpenHelper.KEY_DAY, day);
+                        values.put(StepAppOpenHelper.KEY_HOUR, hour);
+
+                        database.insert(StepAppOpenHelper.TABLE_NAME, null, values);*/
+                    }
+
+                }
+            }
+        }
+
+
+        // Calculate the number of steps from the step detector
+        private void countSteps(float step) {
+
+            //Step count
+            mAndroidStepCounter += (int) step;
+            Log.d("NUM STEPS ANDROID", "Num.steps: " + String.valueOf(mAndroidStepCounter));
+        }
     }
 }
