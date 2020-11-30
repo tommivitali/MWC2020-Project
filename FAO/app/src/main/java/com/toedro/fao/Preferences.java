c60a6f154a128a9c1449da53ead607d252a9bcae
tommivitali <tommivitali@gmail.com>
2020-11-30 17:13:26 +0100

    Notification sent with time set in setting

--- FAO/app/src/main/java/com/toedro/fao/Preferences.java
+++ FAO/app/src/main/java/com/toedro/fao/Preferences.java
@@ -40,14 +40,82 @@ public class Preferences {
         return weightShared;
     }
 
+    private static Integer getValueNotification(int defaultId, int savedKeyId, Context context, Activity activity) {
+        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+
+        Integer defaultNotification = context.getResources().getInteger(defaultId);
+        Integer notificationShared = sharedPref.getInt(context.getString(savedKeyId), defaultNotification);
+
+        return notificationShared;
+    }
+
+    private static Pair<Integer, Integer> getPairHourMinute(int defaultHourId, int defaultMinuteId,
+                                                            int savedKeyHourId, int savedKeyMinuteId,
+                                                            Context context, Activity activity) {
+        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+
+        Integer defaultHour     = context.getResources().getInteger(defaultHourId);
+        Integer defaultMinute   = context.getResources().getInteger(defaultMinuteId);
+        Integer hourShared      = sharedPref.getInt(context.getString(savedKeyHourId), defaultHour);
+        Integer minuteShared    = sharedPref.getInt(context.getString(savedKeyMinuteId), defaultMinute);
+
+        return new Pair<Integer, Integer>(hourShared, minuteShared);
+    }
+
     public static List<Pair<Integer, Integer>> getNotificationsHours(Activity activity, Context context) {
         SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
-        List<Pair<Integer, Integer>> l = new ArrayList<>();
-        l.add(new Pair<>(15, 13));
-        l.add(new Pair<>(14, 40));
-        l.add(new Pair<>(20, 0));
-        l.add(new Pair<>(15, 11));
+        List<Pair<Integer, Integer>> notificationList = new ArrayList<>();
 
-        return l;
+        // 1st notification
+        if(getValueNotification(
+                R.integer.saved_notification_1_default_key, R.string.saved_notification_1_saved_key,
+                context, activity) == 1) {
+            notificationList.add(getPairHourMinute(
+                    R.integer.saved_hour_1_default_key, R.integer.saved_minute_1_default_key,
+                    R.string.saved_hour_1_saved_key, R.string.saved_minute_1_saved_key,
+                    context, activity));
+        }
+
+        // 2nd notification
+        if(getValueNotification(
+                R.integer.saved_notification_2_default_key, R.string.saved_notification_2_saved_key,
+                context, activity) == 1) {
+            notificationList.add(getPairHourMinute(
+                    R.integer.saved_hour_2_default_key, R.integer.saved_minute_2_default_key,
+                    R.string.saved_hour_2_saved_key, R.string.saved_minute_2_saved_key,
+                    context, activity));
+        }
+
+        // 3rd notification
+        if(getValueNotification(
+                R.integer.saved_notification_3_default_key, R.string.saved_notification_3_saved_key,
+                context, activity) == 1) {
+            notificationList.add(getPairHourMinute(
+                    R.integer.saved_hour_3_default_key, R.integer.saved_minute_3_default_key,
+                    R.string.saved_hour_3_saved_key, R.string.saved_minute_3_saved_key,
+                    context, activity));
+        }
+
+        // 4th notification
+        if(getValueNotification(
+                R.integer.saved_notification_4_default_key, R.string.saved_notification_4_saved_key,
+                context, activity) == 1) {
+            notificationList.add(getPairHourMinute(
+                    R.integer.saved_hour_4_default_key, R.integer.saved_minute_4_default_key,
+                    R.string.saved_hour_4_saved_key, R.string.saved_minute_4_saved_key,
+                    context, activity));
+        }
+
+        // 5th notification
+        if(getValueNotification(
+                R.integer.saved_notification_5_default_key, R.string.saved_notification_5_saved_key,
+                context, activity) == 1) {
+            notificationList.add(getPairHourMinute(
+                    R.integer.saved_hour_5_default_key, R.integer.saved_minute_5_default_key,
+                    R.string.saved_hour_5_saved_key, R.string.saved_minute_5_saved_key,
+                    context, activity));
+        }
+
+        return notificationList;
     }
 }
